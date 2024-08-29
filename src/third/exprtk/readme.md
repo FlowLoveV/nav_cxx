66b2db56c025cf1468ccc69883884680209f5ba4
mashuo <shuoma4k@gmail.com>
2024-08-29 21:04:53 +0800

    add exprtk lib and soem requires in xmake.lua

---
+++ src/third/exprtk/readme.md
@@ -0,0 +1,5 @@
+# Introduction
+
+Exprtk is a single-header-only(about 3.7w lines) library,be careful when you include it.
+
+[doc](https://github.com/ArashPartow/exprtk/blob/master/readme.txt)
---
+++ src/third/xmake.lua
@@ -0,0 +1,124 @@
+add_rules("mode.debug", "mode.release")
+
+-- third
+add_requires("toml++",{
+    version = "3.4.0",
+    configs = {
+        shared = true,
+        cppstd = "c++23",
+    }
+})
+add_requires("spdlog",{
+    version = ">=1.12.0",
+    configs = {
+        shared = true,
+        cppstd = "c++20",
+        defines = { "SPDLOG_USE_STD_FORMAT" } -- Add macro definition to enable std::format
+    }
+})
+add_requires("magic_enum", {
+    version = "0.9.5",
+    configs = {
+        shared = true,
+        cppstd = "c++23"
+    }
+})
+add_requires("matplotplusplus", {
+    version = "1.2.1",
+    configs = {
+        shared = true,
+        cppstd = "c++23"
+    }
+})
+add_requires("gflags",{
+    version = "2.2.2",
+    configs = {
+        shared = true,
+        cppstd = "c++23"
+    }
+})
+add_requires("fast_float",{
+    version = "6.1.1",
+    configs = {
+        shared = true,
+        cppstd = "c++23"
+    }
+})
+add_requires("date",{
+    version = "3.0.1",
+    configs = {
+        shared = true,
+        cppstd = "c++23"
+    }
+})
+add_requires("proj",{
+    -- version = "9.4.1",
+    configs = {
+        shared = true,
+        cppstd = "c++23"
+    }
+})
+add_requires("stringzilla",{
+    version = "v3.9.2",
+    configs = {
+        shared = true,
+        cppstd = "c++23",
+    }
+})
+add_requires("boost")
+
+-- Define a recursive function to traverse all directories and add them to includedirs
+function recursive_add_includedirs(dir, is_public)
+    for _, filepath in ipairs(os.dirs(path.join(dir, "*"))) do
+        if os.isdir(filepath) then
+            if is_public then
+                add_includedirs(filepath, {public = true})
+            else
+                add_includedirs(filepath)
+            end
+            recursive_add_includedirs(filepath, is_public)
+        end
+    end
+end
+
+-- [install](http://ceres-solver.org/installation.html#getting-the-source-code)
+-- [source](http://ceres-solver.org/ceres-solver-2.2.0.tar.gz)
+-- platform Ubuntu 24.04
+-- use `dpkg -L libsuitesparse-dev` to show where the apt library location is.
+-- when you want to link ceres-solver,use add_deps_ceres(target)
+function add_deps_ceres(target)
+    -- apt library location 
+    add_linkdirs("/usr/lib/x86_64-linux-gnu")
+    -- ceres-solver library location
+    add_linkdirs("/usr/local/lib")
+    add_links("ceres")
+    -- link deps
+    -- use xmake-repo gflags instead of apt installed or self-built
+    add_packages("gflags")
+    add_links("glog")
+    add_links("blas","lapack")
+    add_links("cholmod", "amd", "colamd", "camd", "ccolamd", "suitesparseconfig")
+end
+
+
+-- reflect-cpp library,build from source directly
+-- add_requires("reflect-cpp") don't work(2024-6-10)
+target("reflect-cpp")
+    set_kind("shared")
+    set_languages("c++23")
+    add_includedirs("reflect-cpp/include",{public = true})
+    add_files("reflect-cpp/src/yyjson.c")
+    add_packages("toml++",{public = true})
+target_end()
+
+
+target("exprtk")
+    set_kind("phony")
+    add_includedirs("exprtk",{public = true})
+    add_linkdirs("exprtk",{public = true})
+target_end()
+
+
+
+
+
